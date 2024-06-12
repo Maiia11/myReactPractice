91cfd66ad684ef49c7fb9c3a14be6e34350db6da
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-06-12 23:31:02 +0300

    add store

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,31 @@
 import { Button } from '../Button/Button';
 import css from './StatusFilter.module.css';
 
+// Імпортуємо хук
+import { useSelector } from "react-redux";
+// Імпортуємо об'єкт значень фільтра
+import { statusFilters } from "../../redux/constsnts";
+
+import { getStatusFilter } from '../../redux/selectors';
+
 export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+ 
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button 
+        selected={filter === statusFilters.all}>
+          All
+      </Button>
+      <Button 
+        selected={filter === statusFilters.active}>
+          Active
+      </Button>
+      <Button 
+        selected={filter === statusFilters.completed}>
+          Completed
+      </Button>
     </div>
   );
 };
